d9c1c4f2ffb44b9842888311506733d9e8bdb641
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2021-03-21 17:13:05 -0300

    Moved logic Sidebar from app to a component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import { GenreResponseProps } from '../App'
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
 }
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
+};
